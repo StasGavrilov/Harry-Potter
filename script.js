b33d9ad7f89e31dbfc29002573cb0d1495b01f79
Stas Gavrilov <stasgavrilov485@gmail.com>
2021-11-22 15:28:38 +0200

    done

--- script.js
+++ script.js
@@ -46,11 +46,11 @@ function getWinDays(extraValue) {
 // fourthBook = 683
 // biggestWinPerDay = 107
 // biggestWinDays = 203
-// booksLeft = 1
-// pageLeft = 132 (14/11/2021)
+// booksLeft = 0
+// pageLeft = 0 (22/11/2021)
 
 function counters() {
-    const page = -27
+    const page = 6
     const totalDays = 103
     const totalPages = 2737
     const pagesPerDay = totalPages / totalDays
